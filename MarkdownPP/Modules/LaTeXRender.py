31d04d73b25edcc3af7ec202aa7de058989d4014
strikles <58505677+strikles@users.noreply.github.com>
2022-05-08 22:02:21 +0200

    Update LaTeXRender.py

--- MarkdownPP/Modules/LaTeXRender.py
+++ MarkdownPP/Modules/LaTeXRender.py
@@ -110,8 +110,8 @@ class LaTeXRender(Module):
                    "Accept": "text/plain"}
 
         # Make the request
-        with closing(HTTPConnection("https://math.vercel.app")) as conn:
-            conn.request("POST", "", params, headers)
+        with closing(HTTPConnection("https://math.vercel.app", 80)) as conn:
+            conn.request("POST", "/", params, headers)
             response = conn.getresponse()
             img_url = response.read()
 
